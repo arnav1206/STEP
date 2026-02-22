2656a69ef27e015f6effe55a3af04e31ed2accf4
arnav1206 <92998894+arnav1206@users.noreply.github.com>
2026-02-22 23:34:22 +0530

    Fix banner output in BANNER_APP.java

--- BANNER_APP.java
+++ BANNER_APP.java
@@ -1,6 +1,6 @@
 public class BANNER_APP {
     public static void main(String[] args) {
-        System.out.println(String.join("   " , "   ***   " , "   ***   " , "  *****  ", " ****** "));
+        System.out.println(String.join("   " , "   ***   " , "   ***   " , " ******  ", " ****** "));
         System.out.println(String.join("   " , " **   ** " , " **   ** " , "**    ** ", "**    **"));
         System.out.println(String.join("   " , "**     **" , "**     **" , "**    ** ", "**      "));
         System.out.println(String.join("   " , "**     **" , "**     **" , "*******  ", " ****** "));
