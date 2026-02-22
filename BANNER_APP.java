9d351305222e272eb0632352286a28c69c1584e1
arnav1206 <arnavgoel1206@gmail.com>
2026-02-22 22:15:10 +0530

    UC03

---
+++ BANNER_APP.java
@@ -0,0 +1,16 @@
+class BANNER_APP
+{
+    public static void main(String args[])
+    {
+        System.out.println(String.join(" " , " *******  " , "  *******  " , "  *******  " , "  *******  "));
+        System.out.println(String.join(" " , "**     ** " , " **     ** " , " **     ** " , " **     ** "));
+        System.out.println(String.join(" " , "**     ** " , " **     ** " , " **     ** " , " **        "));
+        System.out.println(String.join(" " , "**     ** " , " **     ** " , " **     ** " , " **        "));
+        System.out.println(String.join(" " , "**     ** " , " **     ** " , " **     ** " , " **        "));
+        System.out.println(String.join(" " , "**     ** " , " **     ** " , " ********  " , "  *******   "));
+        System.out.println(String.join(" " , "**     ** " , " **     ** " , " **        " , "        ** "));
+        System.out.println(String.join(" " , "**     ** " , " **     ** " , " **        " , "        ** "));
+        System.out.println(String.join(" " , "**     ** " , " **     ** " , " **        " , " **     ** "));
+        System.out.println(String.join(" " , " *******  " , "  *******  " , " **        " , "  *******  "));
+     }
+} 
